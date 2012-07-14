83aec4c521c3723c0819c2e7cbcfe10cadc01e38
david <davicitora@gmail.com>
2012-07-14 13:54:54 -0500

    mensaje

---
+++ practica01/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practica01::Application.config.secret_token = 'ebf4e0e3e8e3e9ac876d90bacfc9c9ddcd6f446abf3b7692769e02b29352ae5b3e22f3d60ed482b0281896eeeb600087833ba3024a140b3b349fa8773b9bfe6b'
